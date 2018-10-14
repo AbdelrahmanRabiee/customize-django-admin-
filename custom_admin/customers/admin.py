d1fd80898cc703672abee2c532046b9119d5e2fd
AbdelrahmanRabiee <abdelrahman.sico_931@fci.helwan.edu.eg>
2018-10-14 19:02:08 +0200

    custom filter in customers app

--- custom_admin/customers/admin.py
+++ custom_admin/customers/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.db.models import Count
 from . import models
 # Register your models here.
+
+class BigOrderFilter(admin.SimpleListFilter):
+    title = 'Big Purchase Total'
+    parameter_name = 'big_purchase'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1','more than 500'),
+            ('2', 'more than 1000'),
+            ('3', 'more than 5000'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '1':
+            return queryset.filter(total__gte=500)
+        elif self.value() == '2':
+            return queryset.filter(total__gte=1000)
+        elif self.value() == '3':
+            return queryset.filter(total__gte=5000)
+
+        return queryset
+
+
+
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['customer_name', 'name', 'email', 'street_1', 'city', 'state', 'country']
     list_editable = ['street_1', ]
@@ -10,7 +35,7 @@ class PurchaseAdmin(admin.ModelAdmin):
     list_display = ['customer', 'customer_name', 'placed_at', 'shipped', 'shipped_at', 'total']
     ordering = ['placed_at',]
     list_editable = ['shipped', 'shipped_at']
-    list_filter = ['shipped', 'total']
+    list_filter = ['shipped', BigOrderFilter]
 
 
 class PurchaseItemAdmin(admin.ModelAdmin):
